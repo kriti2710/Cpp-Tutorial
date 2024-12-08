e83ffcc9c3b6f690ffcdd863b0dd51199df1aed1
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2024-12-09 03:20:32 +0530

    Update cin_and_cout.cpp

--- cin_and_cout.cpp
+++ cin_and_cout.cpp
@@ -1,9 +1,15 @@
 # include <iostream>
+# include <string>
 using namespace std;
 
 int main() {
   int age;
+  cout<<"Enter your age: ";
   cin>>age;
-  cout<<"Your age is "<<age;
+  string name;
+  cout<<"Enter your name: ";
+  getline(cin, name);
+  cout<<"Your age is "<<age<<endl;
+  cout<<"Your name is "<<name<<endl;
   return 0;
 }
